0dad236521349b6044452c6e6ae530ed30bb7e20
gutiab <gutiab@cs.wallawalla.edu>
2024-03-13 15:25:27 -0700

    fantasy story

---
+++ Project/fantasy_story.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void fantasy_story(){
+    char choice;
+    cout<< "You are asked to try a new video game at your friend’s house. The game is called something. As soon as you boot the game up, you find yourself inside the game itself. You have no idea how you got there. You cannot find your friend. You find yourself in the armory of a castle. Everyone around you is putting on armor and gathering weapons. Someone taps you on the shoulder and tells you to put on the armor and to get a weapon. “What platoon are you a part of? A or B?” "<< endl;
+    cout << "\t1 - raiding a dragon hoard" << endl;
+    cout << "\t2 - finding artifact for great wizard" << endl;
+    cout << "Choose '1' or '2'" << endl;
+
+    
+    
+    
+}
+
+void dragon_hoard(){
+ cout << "The kingdom is facing an economic crisis. To solve this, the king has sent your platoon to raid a dragon hoard to bring back whatever riches could help the kingdom. You and your platoon set off on a journey and find the cavern where the dragon lives. You enter the cavern, and you have two options. Go left or right:"<< endl;
+ cout<< "What would you like to do?" << endl;
+ cout << "\tEnter '1' to go left" << endl;
+ cout << "\tEneter '2' to go right" << endl;
+ 
+}
--- Project/main.cpp
+++ Project/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include "fantasy_story.cpp"
+
 
 using namespace std;
 /*
@@ -8,6 +10,22 @@ Two stories. Continual forking style.
 
 int main(){
     string name;
-    cout << "Type out your character's name: "
-    cin >> name;
+    string choice = "y";
+    cout << "Type out your character's name: ";
+    getline(cin,name);
+
+    
+    while (choice == "y") {
+        cout << "Are you sure you want this to be your name (click y to change name)? ";
+        cin >> choice;
+
+        if(choice =="y"){
+        cout << "What would you like to change your name to: ";
+        cin >> name;
+        cout << "Your name is " << name << endl;
+      } else {
+        cout << "Your name is " << name << endl;
+    }
+    }
+   
 }
