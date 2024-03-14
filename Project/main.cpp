90d69a09c3f2b674c9eabbf4672fc953a4a9d1e3
thomje <thomje@cs.wallawalla.edu>
2024-03-14 13:05:23 -0700

    autosave

--- Project/main.cpp
+++ Project/main.cpp
@@ -8,6 +8,21 @@ Two stories. Continual forking style.
 
 int main(){
     string name;
-    cout << "Type out your character's name: "
+    cout << "Type out your character's name: ";
     cin >> name;
+    int choice;
+    cout << "\nchoose the story you want. [lists story options]";
+    cin >> choice;
+    switch (choice){
+        case 1:
+            //story 1
+            break;
+        case 2:
+            //story 1
+            break;
+        default:
+            cout << "invalid option";
+            break;
+    }
+    
 }
