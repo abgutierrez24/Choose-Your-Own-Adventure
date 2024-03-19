7af87fc73b0208a3adf6137302301531e77c2b60
thomje <thomje@cs.wallawalla.edu>
2024-03-18 18:15:25 -0700

    autosave

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,5 +1,11 @@
 #include "Game.cpp"
+#include "ArkenstoneFunctions.h"
 
 int main(){
+    int again;
+    do {
     storyBegin();
+    std::cout << "Would you like to play again?\n Enter 1 if yes, enter 2 if no: " << std::endl;
+    again = input();
+    } while (again == 1);
 }
